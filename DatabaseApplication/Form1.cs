79641ceab4d579fc6488af1b1de01046661b4291
Oscar-Song <oscarcsong96@gmail.com>
2018-08-26 17:53:36 -0700

    ok...

---
+++ DatabaseApplication/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DatabaseApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void connBtn_Click(object sender, EventArgs e)
+        {
+            string connectionString;
+            SqlConnection cnn;
+
+        }
+    }
+}
